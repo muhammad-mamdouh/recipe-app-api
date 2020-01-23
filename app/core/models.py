3edae55eb24dcdebde6dad618b26c09302c30967
Mohammad Mamdouh <mohamedmamdouh@paymobsolutions.com>
2020-01-23 11:14:57 +0200

    Feature: Added custom user model to the core app

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Default manager for User model"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and saves a new user"""
+        if not email:
+            raise ValueError("User must have an email address")
+        email = self.normalize_email(email)
+        user  = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and saves a new super user"""
+        user              = self.create_user(email, password, **extra_fields)
+        user.is_superuser = True
+        user.is_staff     = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(PermissionsMixin, AbstractBaseUser):
+    """Custom user model that supports using email instead of username"""
+    email     = models.EmailField(max_length=255, unique=True)
+    name      = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff  = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD  = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        """Returns string representation of User model objects"""
+        return self.email
