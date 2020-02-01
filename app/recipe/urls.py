1b9049139b45056966de8ffaff094bbefc02c6c7
Mohammad Mamdouh <mohamedmamdouh@paymobsolutions.com>
2020-02-01 17:30:32 +0200

    Feature: Added create new ingredient API endpoint

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import TagViewSet
+from .views import TagViewSet, IngredientsViewSet
 
 
 router = DefaultRouter()
 router.register('tags', TagViewSet)
+router.register('ingredients', IngredientsViewSet)
 
 app_name = 'recipe'
 
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,9 +2,9 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
-from .serializers import TagSerializer
+from .serializers import TagSerializer, IngredientSerializer
 
 
 class TagViewSet(
@@ -23,3 +23,15 @@ class TagViewSet(
     def perform_create(self, serializer):
         """Create a new tag"""
         serializer.save(user=self.request.user)
+
+
+class IngredientsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage ingredients in the database"""
+    serializer_class = IngredientSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+    queryset = Ingredient.objects.all()
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
