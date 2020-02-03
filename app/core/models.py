45118fbd0bdffd4af043c582c9f47b24ed92239d
Mohammad Mamdouh <mohamedmamdouh@paymobsolutions.com>
2020-02-03 22:44:22 +0200

    Feature: Added Recipe model with its corresponding test cases

--- app/core/models.py
+++ app/core/models.py
@@ -64,3 +64,17 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
