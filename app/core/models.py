28b0cb7f187a8505bb9966e710d755f9d0fdcf72
Mohammad Mamdouh <mohamedmamdouh@paymobsolutions.com>
2020-02-14 18:34:34 +0200

    Feature: Added image field to the recipe model and its corresponding test case

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,6 @@
+import os
+import uuid
+
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -5,6 +8,14 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.conf import settings
+
+
+def recipe_image_file_path(instance, filename):
+    """Generate file path for new recipe image"""
+    image_extension = filename.split('.')[-1]
+    new_filename = f'{uuid.uuid4()}.{image_extension}'
+
+    return os.path.join('uploads/recipe/', new_filename)
 
 
 class UserManager(BaseUserManager):
@@ -75,6 +86,7 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
