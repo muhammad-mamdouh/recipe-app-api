98951ac03435db4a30f9b1a3424047dcc4cb0f1f
Mohammad Mamdouh <mohamedmamdouh@paymobsolutions.com>
2020-02-14 17:23:48 +0200

    Setups: Added setup to handle image upload

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 my_apps_urlpatterns = [
     path('api/users/', include('users.urls')),
@@ -25,3 +27,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 urlpatterns += my_apps_urlpatterns
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
